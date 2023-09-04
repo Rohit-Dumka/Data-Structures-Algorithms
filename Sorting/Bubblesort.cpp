ce1a76bd67dd6eed72c9e0c38d5b08b29075b8dd
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-09-04 09:19:42 +0530

    Update Bubblesort.cpp

--- Sorting/Bubblesort.cpp
+++ Sorting/Bubblesort.cpp
@@ -27,3 +27,24 @@ int main() {
     for(int i:a)
         cout<<i<<" ";
 }
+//-------------------------3--------------------
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a[]={2,6,8,10,12};
+    int n=sizeof(a)/sizeof(int);
+    for(int i=0;i<n-1;i++){
+        bool swapped=false;
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                swap(a[j],a[j+1]);
+                swapped=true;
+            }
+        }
+        if(swapped==false)
+            break;
+    }
+    for(int i:a)
+        cout<<i<<" ";
+}
