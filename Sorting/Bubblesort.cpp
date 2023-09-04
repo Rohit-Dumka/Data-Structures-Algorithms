2decfa3e3ac26e40ba0bc80cb3dd7455cab0f557
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-09-04 09:14:50 +0530

    Create Bubblesort.cpp

---
+++ Sorting/Bubblesort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[]={2,10,8,7};
+    int n=sizeof(a)/sizeof(int);
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(a[j]>a[j+1])
+                swap(a[j],a[j+1]);
+        }
+    }
+    for(int i:a)
+        cout<<i<<" ";
+}
