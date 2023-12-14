352585b4ccde9fdf7cc6e14166b5a3110b40cf0d
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-12-14 21:51:47 +0530

    Create 01knapSack.cpp

---
+++ Algos/01knapSack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int knapSack(int w[],int v[],int n,int k){
+    if(n==0 || k==0)
+        return 0;
+    if(w[n-1]>k)
+        return knapSack(w,v,n-1,k);
+    else
+        return max(knapSack(w,v,n-1,k),v[n-1]+knapSack(w,v,n-1,k-w[n-1]));
+}
+int main() {
+    // int w[]={20,60,50,2};
+    // int v[]={100,300,500,40};
+    int v[] = {70, 20, 50};
+    int w[] = {11, 12, 13};
+    int n=sizeof(w)/sizeof(w[0]);
+    int k=30;
+    int ans=knapSack(w,v,n,k);
+    cout<<"max profit: "<<ans;
+}
